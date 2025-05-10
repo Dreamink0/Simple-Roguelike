f8c91998cded790760ec46fc6e35cbe7d8cb54f6
Dreamink0 <Dream_ink@qq.com>
2025-05-10 23:47:55 +0800

    edit testmap

---
+++ assets/Maps/Air.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Air" tilewidth="24" tileheight="24" tilecount="2" columns="2">
+ <image source="Air.png" width="48" height="24"/>
+</tileset>
